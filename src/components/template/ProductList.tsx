c62c7a4f68fbe68ddf916c95f02b2ee08987b611
Erfangh-80 <72462171+Erfangh-80@users.noreply.github.com>
2023-03-10 17:03:52 +0330

    create product list component

---
+++ src/components/template/ProductList.tsx
@@ -0,0 +1,26 @@
+// components
+import Card from "../module/Card";
+// styles
+import styles from "./ProductList.module.css";
+// type product
+import { Product } from "../../types/types";
+
+type productListProps = {
+  text: string;
+  products: Product[]
+};
+
+const ProductList = (props: productListProps): JSX.Element => {
+  return (
+    <>
+      <h1 className={styles.title}>{props.text}</h1>
+      <div className={styles.container}>
+        {props.products.map((product) => (
+          <Card key={product.id} {...product} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ProductList;
