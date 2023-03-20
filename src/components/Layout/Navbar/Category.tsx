2cfc384fe18c0909957ce837c43eba46e75ec36e
Erfangh-80 <72462171+Erfangh-80@users.noreply.github.com>
2023-03-20 12:08:53 +0330

    fetch name category product

--- src/components/Layout/Navbar/Category.tsx
+++ src/components/Layout/Navbar/Category.tsx
@@ -1,40 +1,53 @@
 // router
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+// redux
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../../store/stoe";
+
 // icons
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdCloseCircle } from "react-icons/io";
 // styles
 import styles from "./Category.module.css";
+import { getCategories } from "../../../store/categoriesSlice";
 
 const Category: FC = (): JSX.Element => {
   const [isMenuOpen, setIsMenuOpen] = useState<Boolean>(false);
+  const categoryes = useSelector(
+    (state: RootState) => state.categories.categories
+  );
+  const dispatch = useDispatch<AppDispatch>();
 
   const showMenuHandler = () => {
-    setIsMenuOpen(!isMenuOpen);    
+    setIsMenuOpen(!isMenuOpen);
   };
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, []);
+
+  const category = categoryes.slice(0, 5);
 
   return (
     <div className={styles.container}>
-      <ul className={`${styles.listItem} ${isMenuOpen ? styles.showMenuItem : ""}`}>
-        <button className={styles.btnCloseMenu} onClick={() => setIsMenuOpen(false)}>
+      <ul
+        className={`${styles.listItem} ${
+          isMenuOpen ? styles.showMenuItem : ""
+        }`}
+      >
+        <button
+          className={styles.btnCloseMenu}
+          onClick={() => setIsMenuOpen(false)}
+        >
           <IoMdCloseCircle className={styles.iconClose} />
         </button>
-        <li className={styles.item}>
-          <Link to="">ropa</Link>
-        </li>
-        <li className={styles.item}>
-          <Link to="">Electronics</Link>
-        </li>
-        <li className={styles.item}>
-          <Link to="">Furniture</Link>
-        </li>
-        <li className={styles.item}>
-          <Link to="">Shoes</Link>
-        </li>
-        <li className={styles.item}>
-          <Link to="">Others</Link>
-        </li>
+        {category.map((item) => (
+          <li key={item.id} className={styles.item}>
+            <Link to={`/filter-product/${item.id}`}>{item.name}</Link>
+          </li>
+        ))}
+        
       </ul>
       <button className={styles.btnToggleMenu} onClick={showMenuHandler}>
         <GiHamburgerMenu className={styles.hamburgerMenu} />
