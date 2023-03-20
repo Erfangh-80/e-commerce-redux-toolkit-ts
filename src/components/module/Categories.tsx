c5b94d3ecbfe0f2fd4aede43cb17ff5b469e6ba0
Erfangh-80 <72462171+Erfangh-80@users.noreply.github.com>
2023-03-20 12:16:05 +0330

    added lick to filter product components

--- src/components/module/Categories.tsx
+++ src/components/module/Categories.tsx
@@ -1,17 +1,17 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux/es/exports";
-import { fetchCategories } from "../../store/categorySlice";
+import { Link } from "react-router-dom";
+import { getCategories } from "../../store/categoriesSlice";
 import { AppDispatch, RootState } from "../../store/stoe";
 import styles from "./Categories.module.css";
 
 const Categories = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const { categories } = useSelector((state: RootState) => state.category);
+  const { categories } = useSelector((state: RootState) => state.categories);
   const categoriesData = categories.slice(0, 4);
-  console.log(categoriesData);
 
   useEffect(() => {
-    dispatch(fetchCategories());
+    dispatch(getCategories());
   }, []);
 
   return (
@@ -19,7 +19,9 @@ const Categories = () => {
       {categoriesData.map((category) => (
         <div className={styles.category} key={category.id}>
           <img src={category.image} alt={category.name} />
-          <h3>{category.name}</h3>
+          <Link to={`/filter-product/${category.id}`}>
+            <h3>{category.name}</h3>
+          </Link>
         </div>
       ))}
     </div>
