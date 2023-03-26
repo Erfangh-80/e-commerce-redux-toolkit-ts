773b6734632edf3630473c933521cabe27d846e0
Erfangh-80 <72462171+Erfangh-80@users.noreply.github.com>
2023-03-26 23:08:36 +0430

    show item counter product shoping cart

--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -6,16 +6,24 @@ import { IoSearch } from "react-icons/io5";
 import styles from "./Navbar.module.css";
 // components
 import Category from "./Category";
+// route
 import { Link } from "react-router-dom";
+// redux
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store/stoe";
 
 const Navbar: FC = (): JSX.Element => {
+  const { itemsCounter } = useSelector((state: RootState) => state.cart);
+
   return (
     <>
       <nav className={styles.container}>
         <div className={styles.logo}>
-          <h1>
-            Shopping<span>Hub.</span>
-          </h1>
+          <Link to="/">
+            <h1>
+              Shopping<span>Hub.</span>
+            </h1>
+          </Link>
         </div>
         <div className={styles.search}>
           <input type="text" placeholder="Search here..." />
@@ -27,7 +35,7 @@ const Navbar: FC = (): JSX.Element => {
           <Link to="/shopping-card">
             <FaShoppingCart className={styles.shopIcon} />
             <p>Cart</p>
-            <span>0</span>
+            <span>{itemsCounter}</span>
           </Link>
         </div>
       </nav>
